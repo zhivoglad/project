8e824f2a882a9b1e46fe3bb1347df58926cfcd1c
Vlad <zhivoglad23@gmail.com>
2022-03-30 12:49:15 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let incr = 10,
+	decr = 10;
+//++incr; // префиксн, число сразу меняется, 
+//--decr; // префиксн, число сразу меняется 
+//console.log(incr);
+//console.log(decr);
+
+console.log(incr++);
+console.log(decr--);
+
+
+
+
+
+
+
